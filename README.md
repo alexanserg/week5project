b0ac03feee6b8ef70f0ab40f69db3d609766bbfa
Alexander Sergejev <alexandersergejev@gmail.com>
2019-06-28 10:53:44 -0700

    begin flexhing out specs and tests

---
+++ README.md
@@ -0,0 +1,2 @@
+### specs:
+#### Using a given birth date, code will return age and life expectancy in solar years for Earth, Mercury, Venus, Mars, and Jupiter. If life expectancy has been surpassed, the code will calculate by how much. 
---
+++ spec/spaceAge-spec.js
@@ -0,0 +1,17 @@
+import { spaceAge } from "../src/spaceAge.js"
+
+describe("spaceAge") function () {
+  it('should test that age in earth years are returned correctly', function() {
+    expect(earthAge(date)).toEqual(earthAge);
+  }) {
+    it('should test that life expectancy in earth years are returned correctly', function() {
+      expect(earthExpectancy(date)).toEqual(earthExpectancy);
+     {
+    it('should test that age in mercury years are returned correctly', function() {
+      expect(mercuryAge(date)).toEqual(mercuryAge);
+    }) {
+      it('should test that life expectancy in earth years are returned correctly', function() {
+        expect(mercuryExpectancy(date)).toEqual(mercuryExpectancy);
+      })
+
+}
