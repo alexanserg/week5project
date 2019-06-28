63a0ebf5634b4417009cf1da77f321ea5655c224
Alexander Sergejev <alexandersergejev@gmail.com>
2019-06-28 13:42:57 -0700

    test and create age function, add to spaceAge class

---
+++ src/spaceAge.js
@@ -0,0 +1,8 @@
+export class spaceAge {
+  constructor(birthDate, todaysDate, earthAge, expect, merYears, venYears, marYears, jupYears) {
+    this.birthdate = inputtedBirthDate;
+    this.todaysDate = new Date().toISOString().slice(0, 10)
+    this.earthAge = presentDate - birthDate;
+    this.expect = averageLife-earthAge;
+  }
+}
